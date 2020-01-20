86280ea218b26ccd52ab59bc6e0c908cb662c3d4
jinsu6628 <jinsu6628@gmail.com>
2020-01-20 10:03:53 +0900

    2020/01/20

---
+++ Backjoon/Main10709_기상캐스터.java
@@ -0,0 +1,44 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main10709_기상캐스터 {
+
+	/*
+	 * 1. 구름이 언제오는지 출력
+	 * 
+	 * 2. H,W <=100
+	 * 
+	 * 3. c가나오면 0부터 시작
+	 */
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int[][] map = new int[H][W];
+		
+		for (int i = 0; i < H; i++) {
+			String input = br.readLine();
+			int idx = -1;
+			for (int j = 0; j < W; j++) {
+				if(input.charAt(j) == 'c') {
+					idx = 0;
+				}
+				map[i][j] = idx;
+				if(idx !=-1)	idx++;
+			}
+		}
+		
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
--- Backjoon/Main1756_피자굽기.java
+++ Backjoon/Main1756_피자굽기.java
@@ -1,6 +1,10 @@
 package Backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main1756_피자굽기 {
 
@@ -13,55 +17,46 @@ public class Main1756_피자굽기 {
 	 * 3-1.반죽의 배열 arr[], 피자가 들어가면 pizza[]
 	 * 3-2. 피자반죽이 배열의 숫자보다 작으면  그 전 인덱스를 트루로 만들어주고
 	 * 3-3. 0번쨰부터 true인 것을 찾으면 끝
-	 * -> 시간초과
+	 * -> 시간초과 (n^2)
 	 * 
-	 * 3-1 뒤부터 시작 ( 수정 )
+	 * 3-1. 오븐 크기를 전것과 비교해서 작은것만 넣는다
+	 * 3-2. 피자를 넣으면서 해당 위치 저장
+	 * -> n
 	 */
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		int D = sc.nextInt();
-		int N = sc.nextInt();
-		int[] arr = new int[D];
-		int[] pizza = new int[D];
-		int maxarr = 0;
-		int maxpizza = 0;
-		for (int i = 0; i < D; i++) {
-			arr[i] = sc.nextInt();
-			maxarr = Math.max(maxarr, arr[i]);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		int D = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int[] oven = new int[D];
+		int[] pizza = new int[N];
+		
+		//작은 것과 비교해서 어짜피 못들어가기 떄문에 작은 값 저장
+		st = new StringTokenizer(br.readLine());
+		oven[0] = Integer.parseInt(st.nextToken());
+		for (int i = 1; i < D; i++) {
+			oven[i] = Math.min(oven[i-1], Integer.parseInt(st.nextToken()));
 		}
+		
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			pizza[i] = sc.nextInt();
-			maxpizza = Math.max(maxpizza, pizza[i]);
+			pizza[i] = Integer.parseInt(st.nextToken());
 		}
-		int ans =-1;
-		int tmp = 0;
-		int idx = N-1;
-		//최고보다 크면 다 안들어가지므로 끝내기 
-		if(maxpizza > maxarr)System.out.println(0);
-		else {
-		//맨 끝의 숫자보다 가장 큰곳 찾기
-		for (int j = 0; j < D; j++) {
-			if(pizza[N-1] >arr[j]) {
-				ans = j-1;
-				tmp = j;
+		
+		int idx =0;
+		int ans = 0;
+		
+		for (int i = D-1; i >=0; i--) {
+			if(oven[i] >= pizza[idx]) {
+				idx++;
+			}
+			
+			if(idx == N) {
+				ans = i+1;
 				break;
 			}
 		}
 		
-		//만약 못찾았으면 없는 것이므로 0
-		if(ans == -1)System.out.println(0);
-		else {
-			//그 전에 몇개가 쌓여있는지 판단
-			for (int i = N-2; i >=0 ; i--) {
-				if(pizza[i] > arr[tmp]) {
-					ans -= idx - i;
-					idx = i;
-				}else;//없으면 종료
-			}
-			//맨 위보다 더내려갔으면 가장 위이므로 1
-			if(ans < 1)System.out.println(1);
-			else System.out.println(ans+1);
-		}
-		}
+		System.out.println(ans);
 	}
 }
