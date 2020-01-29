7673fb2aa8d00b78bc41a7d74bca2f4747bec4b3
jinsu6628 <jinsu6628@gmail.com>
2020-01-29 15:10:39 +0900

    2020/01/28

---
+++ Programs/Solution_쿠팡1.java
@@ -0,0 +1,44 @@
+package Programs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_쿠팡1 {
+
+	/*
+	 * 1. 배송하지 않은 집의 위치를 오른차순 정렬
+	 * 
+	 * 2. 3<= n <=100000,  -1000000000<min,max< 1000000000 십억
+	 * 
+	 * 3. boolean[] 배열
+	 */
+	public static void main(String[] args) {
+		int n = 5;
+		int min_position = -5;
+		int max_position = 3;
+		int[] positions = {-1,-3,3};
+		System.out.println(Arrays.toString(solution(n,min_position,max_position,positions)));
+	}
+
+	private static int[] solution(int n, int min_position, int max_position, int[] positions) {
+		int[] answer;
+		
+		Map<Integer,Boolean> map = new HashMap<>();
+		for (int i = 0; i < positions.length; i++) {
+			map.put(positions[i], true);
+		}
+		int idx = (max_position - min_position)/(n-1);
+		int start = min_position;
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = min_position; i <= max_position; i+=(max_position - min_position)/(n-1)) {
+			if(!map.containsKey(i))list.add(i);
+		}
+		answer = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = list.get(i);
+		}
+		return answer;
+	}
+}
---
+++ Programs/Solution_쿠팡2.java
@@ -0,0 +1,66 @@
+package Programs;
+
+public class Solution_쿠팡2 {
+
+	/*
+	 * 1. 조건에 맞게 물건을 뱅송하는 방법의 수
+	 * 
+	 * 2. 1<=N<=6, 1<=M<=20, T<=N,K<=M
+	 * 
+	 * 3. dfs
+	 */
+	static int[] arr = new int[6];
+	static int answer;
+
+	public static void main(String[] args) {
+		int N = 4;
+		int M = 7;
+		int T = 2;
+		int K = 3;
+		System.out.println(solution(N, M, T, K));
+	}
+
+	static int solution(int n, int m, int t, int k) {
+		dfs(0, 0, n, m, t, k);
+		return answer;
+	}
+	static void dfs(int idx, int sum, int n, int m, int t, int k) {
+		if (idx == n) {
+			if (chk(n, m, t, k)) {
+				answer++;
+			}
+			return;
+		}
+		for (int i = 0; i <= m - sum; i++) {
+			arr[idx] = i;
+			dfs(idx + 1, sum + i, n, m, t, k);
+		}
+	}
+
+	static boolean chk(int n, int m, int t, int k) {
+		int tmpsum = 0;
+		int sum = 0;
+		for (int i = 0; i < t; i++) {
+			tmpsum += arr[i];
+			sum += arr[i];
+		}
+		if (tmpsum > k) {
+			return false;
+		}
+		for (int i = t; i < n; i++) {
+			tmpsum += arr[i];
+			tmpsum -= arr[i - t];
+			sum += arr[i];
+			if (tmpsum > k) {
+				return false;
+			}
+		}
+		if (sum < m)
+			return false;
+		return true;
+
+	}
+
+	
+
+}
