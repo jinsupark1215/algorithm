5e6dde38a706be5655ce7841bb1650b469777a36
jinsu6628 <jinsu6628@gmail.com>
2020-02-16 23:28:49 +0900

    2020/02/16

---
+++ 다익스트라/Main3385_녹색옷입은애가젤다지.java
@@ -0,0 +1,20 @@
+package 다익스트라;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main3385_녹색옷입은애가젤다지 {
+
+	/*
+	 * 1. 최소 피해량
+	 * 
+	 * 2. N<= 125, 0입력받으면 끝
+	 * 
+	 * 3. 다익스트라 구현
+	 */
+	static int[][] pos = {{1,0},{-1,0},{0,1},{0,-1}};
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
