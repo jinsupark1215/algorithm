7517fbda4e0bdcb720b930ef59e4a455b78ba6bf
jinsu6628 <jinsu6628@gmail.com>
2020-04-01 21:51:21 +0900

    2020/04/01 - [백준] 안테나

--- Backjoon/Main18310.java
+++ Backjoon/Main18310.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main18310 {
@@ -14,64 +13,23 @@ public class Main18310 {
 	 * 
 	 * 2. N <=200,000
 	 * 
-	 * 3. dp? 이분탐색 ?
+	 * 3. 허탈
 	 */
-	static int N,ans;
-	static int[] arr,cnt;
-	static PriorityQueue<Point> q;
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		ans = 0;
-		arr = new int[N];
-		cnt = new int[100001];
-		q = new PriorityQueue<>();
-		boolean flag = true;
-		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			arr[i] = num;
-			cnt[num]++;
-			if(cnt[num] >1)flag = false;
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		if (flag) {
-			Arrays.sort(arr);
-			if (N % 2 == 0) {
-				ans = arr[(N / 2) - 1];
-			} else {
-				ans = arr[N / 2];
-			}
+		Arrays.sort(arr);
+		if (N % 2 == 0) {
+			System.out.println(arr[(N / 2) - 1]);
 		} else {
-			for (int i = 0; i <= 100000; i++) {
-				if(cnt[i] >1)go(i);
-			}
-			ans = q.poll().idx;
-		}
-		System.out.println(ans);
-	}
-
-	private static void go(int idx) {
-		int sum = 0;
-		for (int i = 0; i < N; i++) {
-			sum += Math.abs(arr[i]-idx);
-		}
-		q.add(new Point(idx,sum));
-	}
-	static class Point implements Comparable<Point>{
-		int idx, sum;
-
-		public Point(int idx, int sum) {
-			super();
-			this.idx = idx;
-			this.sum = sum;
-		}
-
-		@Override
-		public int compareTo(Point o) {
-			return this.sum - o.sum;
+			System.out.println(arr[N / 2]);
 		}
 	}
 }
