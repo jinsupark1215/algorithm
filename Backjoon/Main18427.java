d48e45722c3828f4566069734043e92d561cb445
jinsu6628 <jinsu6628@gmail.com>
2020-04-19 23:14:09 +0900

    2020/04/19 - [백준] 함께블록쌓기

--- Backjoon/Main18427.java
+++ Backjoon/Main18427.java
@@ -3,6 +3,9 @@ package Backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main18427 {
@@ -13,9 +16,10 @@ public class Main18427 {
 	 * 2. N,M <=50, H <=1000
 	 * 
 	 * 3. 완탐 ( 시간초과 )
+	 * -> dp
 	 */
 	static int N, M, H, ans;
-	static int[][] map;
+	static ArrayList[] map;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,34 +28,39 @@ public class Main18427 {
 		M = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
 		ans = 0;
-		map = new int[N][M];
-
+		map = new ArrayList[N];
+		int[] dp = new int[H+1];
+		
 		for (int i = 0; i < N; i++) {
+			map[i] = new ArrayList<Integer>();
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				if (st.hasMoreTokens()) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+					map[i].add(Integer.parseInt(st.nextToken()));
 				}
 			}
 		}
+		for (int i = 0; i < N; i++) {
+			Collections.sort(map[i]);
+		}
 		
-		dfs(0, 0, 0);
-		System.out.println(ans%10007);
+		for (int i = 0; i < N; i++) {
+			int[] arr = new int[H+1];
+			for (int j = 0; j < map[i].size(); j++) {
+					int num = (int) map[i].get(j);
+					arr[num]++;
+					for (int k = 1; k <= H; k++) {
+						if(k+num > H)break;
+						arr[num+k] += dp[k];
+				}
+			}
+			for (int j = 1; j <= H; j++) {
+				dp[j] += arr[j];
+				dp[j] %= 10007;
+			}
+		}
+		
+		System.out.println(dp[H]);
 	}
 
-	private static void dfs(int r, int c, int sum) {
-		if(sum == H) {
-			ans++;
-			return;
-		}else if(sum > H || r== N) {
-			return;
-		}else {
-			for (int i = 0; i < M; i++) {
-				if(map[r][i] != 0) {
-					dfs(r+1,0,sum+map[r][i]);
-				}else break;
-			}
-			dfs(r+1,0,sum);
-		}
-	}
 }
