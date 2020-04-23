7fc50b1da01047a39fe2720b4258a7171fe8d771
jinsu6628 <jinsu6628@gmail.com>
2020-04-23 22:58:13 +0900

    2020/04/23 - [백준]최솟값찾기 / 슬라이딩윈도우

--- Backjoon/Main11003.java
+++ Backjoon/Main11003.java
@@ -1,41 +1,49 @@
 package Backjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main11003{
 
+	/*
+	 * [백준] 최솟값 찾기
+	 * 
+	 * 1. 순서대로 출력
+	 * 
+	 * 2. N,L <=5,000,000
+	 * 
+	 * 3. 슬라이딩 윈도우 방법 -> 데크 개념
+	 */
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
-		PriorityQueue<Integer> q = new PriorityQueue<Integer>(new Comparator<Integer>() {
-
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return o1-o2;
-			}
-		});
+		int[] A = new int[N];
+		int[] deque = new int[N];
+		int[] ans = new int[N];
 		
 		st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			q.add(num);
-			if(q.size()<L) {
-				int tmp =q.poll();
-				sb.append(tmp + " ");
-				q.add(tmp);
-			}else {
-				sb.append(q.poll() + " ");
-				
+		int left = 0, right = 0;
+		for (int n = 0; n < N; n++) {
+			A[n] = Integer.parseInt(st.nextToken());
+			while (left != right && A[deque[right - 1]] > A[n]) {
+				right--;
 			}
+			deque[right++] = n;
+			if (deque[left] <= n - L) {
+				left++;
+			}
+			ans[n] = A[deque[left]];
 		}
-		System.out.println(sb.toString());
+		for (int a : ans) {
+			bw.write(a + " ");
+		}
+		bw.close();
 	}
 }
