556a7e47f699462b2a6f757e07a2145ee6f5f5b1
jinsu6628 <jinsu6628@gmail.com>
2021-01-15 21:10:40 +0900

    2021/01/15 - [백준] 선물이 넘쳐흘러

---
+++ Backjoon/Main17259.java
@@ -0,0 +1,26 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main17259 {
+
+	/*
+	 * [백준] 선물이 넘쳐흘러
+	 * 
+	 * 1. 선물 포장 개수
+	 * 
+	 * 2. 
+	 * 
+	 * 3. 구현
+	 */
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int B = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+	}
+}
